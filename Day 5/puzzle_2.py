ba950570366fe50b0bd064486b933681a89cd00a
Lego-RL <joeylbarroso@gmail.com>
2021-12-05 15:09:46 -0600

    final solution puzzle 2

---
+++ Day 5/puzzle_2.py
@@ -0,0 +1,73 @@
+with open('input.txt', 'r') as f:
+    data = f.readlines()
+
+
+def slope(x1, y1, x2, y2):
+
+    m = (y2-y1) / (x2-x1)
+    return m
+
+
+
+map = [ [0] * 1000 for _ in range(1000)]
+
+
+for x in data:
+    left, right = x.split('->')
+
+    left = left.strip()
+    right = right.strip()
+
+    x1, y1 = left.split(',')
+    x2, y2 = right.split(',')
+
+    x1, y1 = int(x1), int(y1)
+    x2, y2 = int(x2), int(y2)
+
+
+    if x1 != x2 and y1 != y2:
+        
+        line_slope = slope(x1, y1, x2, y2)
+
+        if x1 > x2:
+            x1, x2 = x2, x1
+            y1, y2 = y2, y1
+
+        offset = 1 if y2 > y1 else -1
+
+        while x1 != x2+1:
+            map[x1][y1] += 1
+            x1 += 1
+            y1 += offset
+
+
+
+    elif x1 == x2:
+        if y1 < y2:
+            loop_over = range(y1, y2+1)
+
+        else:
+            loop_over = range(y2, y1+1)
+            
+        for i in loop_over:
+            map[x1][i] += 1
+
+    #y1 == y2
+    else:
+        if x1 < x2:
+            loop_over = range(x1, x2+1)
+
+        else:
+            loop_over = range(x2, x1+1)
+
+        for i in loop_over:
+            map[i][y1] += 1
+
+
+count = 0
+for row in map:
+    for element in row:
+        if element > 1:
+            count += 1
+
+print(count)
