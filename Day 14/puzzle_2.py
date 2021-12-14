849fb6e4893df6283ee071f973c388c4bdc1852e
Lego-RL <joeylbarroso@gmail.com>
2021-12-13 23:29:15 -0600

    add puzzle_1.py

---
+++ Day 14/puzzle_2.py
@@ -0,0 +1,40 @@
+from collections import Counter
+
+with open('input.txt', 'r') as f:
+    data = f.read().splitlines()
+
+template = data.pop(0)
+del data[0]
+rules = {}
+
+def step(template):
+
+    inserts = []
+    
+    for i in range(len(template)-1):
+        pair = f'{template[i]}{template[i+1]}'
+        inserts.append(rules[pair])
+
+
+    rtn_template = ''
+    for count, i in enumerate(template):
+        rtn_template += i
+        if len(inserts) > 0:
+            rtn_template += inserts.pop(0)
+        
+
+    return rtn_template
+
+
+
+for rule in data:
+    pair, insertant = rule.split(' -> ')
+    rules[pair] = insertant
+
+for i in range(40):
+    template = step(template)
+
+count = Counter(template)
+
+most_common, least_common = max(count, key=count.get), min(count, key=count.get) 
+print(template.count(most_common) - template.count(least_common))
